951ce44abcb2893ba88364013f5b3298701c96d5
Piotr Gerke <piotrg2003@gmail.com>
2021-06-28 15:36:35 +0200

    finished youtube web application with react

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,14 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({video}) =>{
+const VideoItem = ({video, onVideoSelect}) =>{
     return (
-    <div>
-    <img src={video.snippet.thumbnails.medium.url}></img>
-    {video.snippet.title}
+    <div className="video-item item" onClick={() => onVideoSelect(video)}>
+    <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url}></img>
+    <div className="content">
+      <div className="header">{video.snippet.title}</div>
+    </div>
+
     </div>
     )
 };
